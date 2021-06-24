4c7e852423e970567f1a38dbc160972a19e242bf
misschloeh <79707195+misschloeh@users.noreply.github.com>
2021-06-24 09:31:21 +0100

    Add files via upload

---
+++ BankAccountTutorial/LineOfCreditAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccountTutorial
+{
+    class LineOfCreditAccount : BankAccount
+    {
+        public LineOfCreditAccount(string name, decimal initialBalance, decimal creditLimit) : base(name, initialBalance, -creditLimit)
+        {
+        }
+
+        public override void PerformMonthEndTransactions()
+        {
+            if (Balance < 0)
+            {
+                var interest = -Balance * 0.07m;
+                MakeWithdrawal(interest, DateTime.Now, "Charge monthly interest");
+            }
+        }
+
+        protected override Transaction? CheckWithdrawalLimit(bool isOverdrawn) =>
+        isOverdrawn
+        ? new Transaction(-20, DateTime.Now, "Apply overdraft fee")
+        : default;
+    }
+}
